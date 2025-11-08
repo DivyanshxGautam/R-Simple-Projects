ebca76c052532a237e3769cd9a60e46429ff6012
Divyansh <divyansh7192@gmail.com>
2025-11-08 14:40:24 +0530

    Changes in P3

--- P3.R
+++ P3.R
@@ -1,23 +1,8 @@
-#3. Write a R program to get the first 10 Fibonacci numbers. 
-fibonacci <- function(n) {
-  if (n <= 0) {
-    return(numeric(0))
-  }
-  if (n == 1) {
-    return(c(0))
-  }
-  fib_sequence <- c(0, 1)
-  while (length(fib_sequence) < n) {
-    last_two <- tail(fib_sequence, 2)
-    next_fib <- last_two[1] + last_two[2]
-    fib_sequence <- c(fib_sequence, next_fib)
-  }
-  return(fib_sequence)
-}
+# 3. Write a R program to get the first 10 Fibonacci numbers
 
-num_fibs <- 10
-
-result <- fibonacci(num_fibs)
-
-cat("The first", num_fibs, "Fibonacci numbers are:\n")
-print(result)
+Fibonacci <- numeric(10)
+Fibonacci[1] <- Fibonacci[2] <- 1
+for (i in 3:10)
+  Fibonacci[i] <- Fibonacci[i - 2] + Fibonacci[i - 1]
+print("First 10 Fibonacci numbers:")
+print(Fibonacci)
